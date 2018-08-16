91b6cf3f1414b7db017e61af86ea8ba033c61f8b
Sam Ashtar <samashtarm@gmail.com>
2018-08-16 15:48:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms (array)
 end
 
 def printer (attendees)
-  batch_badge_creator 
-  assign_rooms
+  batch_badge_creator(attendees).each {||}
+  assign_rooms(attendees).each {||}
   
 end 
