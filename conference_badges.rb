31b281732a8a06a1ac536085a8b933e421f3d56d
Sam Ashtar <samashtarm@gmail.com>
2018-08-16 15:42:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,12 +9,11 @@ def batch_badge_creator (array)
 end 
 
 def assign_rooms (array)
-  counter = 1
-  room_assignments = []
-  array.each do |name|
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
-    counter += 1
-  end
-  room_assignments
-  
+ counter = 1 
+ room_assignments = []
+ array.each do |name|
+   room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+   counter +=1
+ 
+ 
 end
