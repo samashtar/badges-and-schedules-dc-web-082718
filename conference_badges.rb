de4f40d16194e7a3be11ef9f7a4489ea43720720
Sam Ashtar <samashtarm@gmail.com>
2018-08-16 15:40:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,12 @@ def batch_badge_creator (array)
 end 
 
 def assign_rooms 
-  
+  counter = 1
+  room_assignments = []
+  attendees.each do |name|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+  room_assignments
   
 end
